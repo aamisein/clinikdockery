5f03daae65f4efd89ea8b13059998bcd3a51e152
aamisein <amirhosein56@gmail.com>
2025-05-20 09:18:44 +0330

    style done finally and start dockery project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
       './templates/**/*.{html,js}',
       './visits/templates/**/*.{html,js}', // مسیر فایل‌های HTML و JS در اپلیکیشن Django
     ],
+    darkMode: 'class',
     theme: {
       extend: {},
     },
