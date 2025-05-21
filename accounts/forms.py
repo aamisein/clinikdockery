e7c08a04502159ecd17dfeb7717772ed32786f7d
aamisein <amirhosein56@gmail.com>
2025-05-21 10:00:52 +0330

    update register form style

--- accounts/forms.py
+++ accounts/forms.py
@@ -40,14 +40,19 @@ class UserRegistrationForm(UserCreationForm):
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'w-full px-4 py-2 pl-10 mt-1 border rounded-lg focus:ring-2 focus:ring-indigo-400 focus:outline-none',
-                'placeholder': 'نام کاربری'
+                'placeholder': 'نام کاربری',
+                'id': 'id_username'
             }),
             'password1': forms.PasswordInput(attrs={
                 'class': 'w-full px-4 py-2 pl-10 mt-1 border rounded-lg focus:ring-2 focus:ring-indigo-400 focus:outline-none',
-                'placeholder': 'رمز عبور'
+                'placeholder': 'رمز عبور',
+                'id': 'id_password1',
+                'style': 'padding-right: 2.5rem;'
             }),
             'password2': forms.PasswordInput(attrs={
                 'class': 'w-full px-4 py-2 pl-10 mt-1 border rounded-lg focus:ring-2 focus:ring-indigo-400 focus:outline-none',
-                'placeholder': 'تکرار رمز عبور'  
+                'placeholder': 'تکرار رمز عبور',
+                'id': 'id_password2',
+                'style': 'padding-right: 2.5rem;'
             }),
         }
